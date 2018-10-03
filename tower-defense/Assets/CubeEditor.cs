1e33bac79abbdda78e02ccf902bca11525742867
N Gibson <nbgibson@users.noreply.github.com>
2018-10-02 23:20:02 -0400

    Implemented TextMP labels, label logic, editor shenanigans

--- tower-defense/Assets/CubeEditor.cs
+++ tower-defense/Assets/CubeEditor.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
-	// Update is called once per frame
-	void Update () {
-        Vector3 snapPos;
+    TextMeshPro textMesh;
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 snapPos;       
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-	}
+
+        textMesh = GetComponentInChildren<TextMeshPro>();
+        textMesh.text = snapPos.x/gridSize + ", " + snapPos.z/gridSize;
+    }
 }
