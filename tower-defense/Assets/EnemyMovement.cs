65ced9b4bebeef9132be6aaab262b5341c228761
N Gibson <nbgibson@users.noreply.github.com>
2018-10-03 22:58:50 -0400

    Coroutines

---
+++ tower-defense/Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> path;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach (Waypoint wayPoint in path)
+        {
+            transform.position = wayPoint.transform.position;
+            print("Visiting: " + wayPoint);
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol");
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
