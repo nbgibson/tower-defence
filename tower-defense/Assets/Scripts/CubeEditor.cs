d886ef871a47bfdf494068b17056dcf78a6553a5
N Gibson <nbgibson@users.noreply.github.com>
2018-10-07 21:51:01 -0400

    Additional world logic and first pathfinding steps

---
+++ tower-defense/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour {
+
+    Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(waypoint.GetGridPos().x * gridSize, 0f, waypoint.GetGridPos().y * gridSize);
+    }
+
+    private void UpdateLabel()
+    {
+        TextMeshPro textMesh = GetComponentInChildren<TextMeshPro>();
+        string labelText = waypoint.GetGridPos().x + ", " + waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+        gameObject.name = "Cube (" + labelText + ")";
+    }
+}
---
+++ tower-defense/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+
+    [SerializeField] Waypoint startWayPoint, stopWayPoint;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+	// Use this for initialization
+	void Start () {
+        LoadBlocks();
+        ColorStartAndEnd();
+        ExploreNeighbors();
+	}
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            if (grid.ContainsKey(waypoint.GetGridPos()))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWayPoint.SetTopColor(Color.green);
+        stopWayPoint.SetTopColor(Color.red);
+    }
+
+    private void ExploreNeighbors()
+    {
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWayPoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                //foo
+            }
+        }
+    }
+}
